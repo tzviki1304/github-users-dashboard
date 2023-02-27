0bda332ccb81276e16c363f52f7f46adc3abd359
tzviki <twilhelm@votis.net>
2023-02-27 10:16:02 +0200

    add env file

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Auth0Provider
-      domain="dev-l6zho6b82q605oyf.us.auth0.com"
-      clientId="demfx6ZtRc3nTHruzEnEqwZ3pYnfYRW0"
+      domain={process.env.REACT_APP_AUTH_DOMAIN}
+      clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
       redirectUri={window.location.origin}
       cacheLocation="localstorage"
     >
